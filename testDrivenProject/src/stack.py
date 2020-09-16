2d1eedaadd3e08d0fa308635dbe6e76945c83cd2
ozgurgundogan <ozgurgundogan90@gmail.com>
2020-09-17 00:50:19 +0300

    init

---
+++ testDrivenProject/src/stack.py
@@ -0,0 +1,18 @@
+
+
+class Stack:
+    def __init__(self):
+        self.elements=[]
+
+    def pop(self):
+        result = self.elements[-1]
+        del self.elements[-1]
+        return result
+
+    def push(self, n):
+        if(isinstance(n,int)):
+            self.elements.append(n)
+
+
+    def __len__(self):
+        return len(self.elements)
---
+++ testDrivenProject/tests/test_stack.py
@@ -0,0 +1,26 @@
+from src.stack import Stack
+
+def test_constructor():
+    s = Stack()
+    assert isinstance(s,Stack) , "Stack can not be constructed."
+
+def test_push():
+    s=Stack()
+    s.push(4)
+    assert len(s)==1, "Value can not be pushed."
+
+def test_push_string():
+    s=Stack()
+    s.push("str")
+    assert len(s)==0, "Value can not be pushed."
+
+def test_push_float():
+    s=Stack()
+    s.push(float(31.1))
+    assert len(s)==0, "Value can not be pushed."
+
+def test_pop():
+    s = Stack()
+    v=4
+    s.push(v)
+    assert s.pop()==v, "Value can not be pushed or popped."
